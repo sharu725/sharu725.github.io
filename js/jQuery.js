32aea404352f1e019fd51922df4aea36d28db27e
Dude <dude@dude.com>
2016-10-27 01:51:02 +0530

    email validation

--- js/jQuery.js
+++ js/jQuery.js
@@ -9,13 +9,11 @@ var ev = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})
             {
                 x.innerHTML	= "Not a valid email";
                 x.style.display = "block";
-                x.style.color = "#960c0c"
             }
         else
             {
                 x.innerHTML	= "Looks good!";
                 x.style.display = "block";
-                x.style.color = "rgba(13, 109, 39, 0.71)"
             }
         };
 
