a33fea6a4b3de2ffa8cf30387ed1097f875e7d3e
Dude <dude@dude.com>
2016-10-26 12:56:13 +0530

    no message

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,2 +1,12 @@
  var contactform =  document.getElementById('theForm');
 contactform.setAttribute('action', '//formspree.io/' + 'sharathdt+webjeda' + '@' + 'gmail' + '.' + 'com');
+
+
+ $('#email_address').on('keyup', function() {
+            var re = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/.test(this.value);
+                if(!re) {
+                    $('#error').show();
+                } else {
+                $('#error').hide();
+    }   
+})
