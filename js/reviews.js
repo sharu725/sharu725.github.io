770d90ec37fe136af8f24da84d4cc2d0b721b7bc
jekyll <jekyll@gmail.com>
2017-10-27 16:16:29 +0530

    new layout

--- js/reviews.js
+++
@@ -1,8 +0,0 @@
- $(document).ready(function() {
-      $("#google-reviews").googlePlaces({
-          placeId: 'ChIJx113w8QUuzsR_3IiWBPrucE'
-        , render: ['reviews']
-        , min_rating: 4
-        , max_rows:5
-      });
-    });
