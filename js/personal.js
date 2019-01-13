27cd484f83eb8fc84dca4b89ebe05885fe3f373b
sharath Kumar <sharath.dt@gmail.com>
2019-01-13 14:34:41 +0530

    spam protection for email and phone added

---
+++ js/personal.js
@@ -0,0 +1,6 @@
+$(".r-phone").click(function(){
+ $(".r-phone").html('<a href="tel:+918904475972">+91 890 447 5972</a>');
+});
+$(".r-email").click(function(){
+ $(".r-email").html('<a href="mailto:hello@webjeda.com">hello@webjeda.com</a>');
+});
